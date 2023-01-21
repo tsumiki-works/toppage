64081c9faad241890ced3838053e907561a863e5
shomsato <satoshoma@watanabemayu.jp>
2023-01-22 00:01:53 +0900

    remove testpage

--- doc/index.php
+++ doc/index.php
@@ -45,8 +45,11 @@
 
 <body class="container max-w-screen-lg mx-auto flex flex-col min-h-screen">
   <main class="flex-grow">
-    <dev class="flex">
-      <aside class="w-56 hidden sm:inline-block ml-2 my-2 text-slate-800" aria-label="Sidebar">
+    <dev class="sm:flex">
+      <section class="m-4 leading-relaxed sm:order-2">
+        <?php include('pageloader.php'); ?>
+      </section>
+      <aside class="w-56 ml-2 my-2 text-slate-800 order-1" aria-label="Sidebar">
         <a class="mx-1" href="https://pyramid.224.works/doc/">
           <h1
             class="inline-flex items-center rounded bg-slate-100 w-52 py-1 pl-2 text-slate-800 hover:opacity-90 text-2xl">
@@ -106,68 +109,6 @@
             組み込み関数</h2>
         </a>
       </aside>
-    <section class="w-fit m-4 leading-relaxed">
-	    <?php include('pageloader.php'); ?>
-      </section>
-    </dev>
-    <dev class="sm:hidden text-slate-800">
-      <a class="mx-1" href="">
-        <h1 class="inline-flex items-center rounded bg-slate-100 m-2 py-2 px-2 text-slate-800 hover:opacity-90 text-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" fill="lightslategray"
-            class="h-7 my-2 ml-2 mr-3">
-            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-            <path
-              d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM80 64h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16s7.2-16 16-16zm16 96H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm0 32v64H288V256H96zM240 416h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H240c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
-          </svg>
-          ドキュメントメニュー
-        </h1>
-      </a>
-      <a href="?page=literal" class="hover:text-slate-700">
-        <h2 class="pl-2 mx-2 mt-4 text-xl border-l-2 border-sky-100 hover:border-sky-400">
-          型</h2>
-      </a>
-      <a href="?page=i32" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">i32</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h2 class="pl-2 mx-2 mt-4 text-xl border-l-2 border-sky-100 hover:border-sky-400">
-          一項演算子</h2>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">論理否定</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h2 class="pl-2 mx-2 mt-4 text-xl border-l-2 border-sky-100 hover:border-sky-400">
-          二項演算子</h2>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">加算</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">減算</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">乗算</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">除算（切り捨て）</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">あまり演算</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">累乗演算</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">論理積</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h3 class="pl-2 mx-2 mt-3 text-lg border-l-2 border-sky-100 hover:border-sky-400 ml-6">論理和</h3>
-      </a>
-      <a href="#" class="hover:text-slate-700">
-        <h2 class="pl-2 mx-2 mt-4 text-xl border-l-2 border-sky-100 hover:border-sky-400">
-          組み込み関数</h2>
-      </a>
     </dev>
   </main>
 </body>
